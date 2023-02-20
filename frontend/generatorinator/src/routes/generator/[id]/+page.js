33b8baa9bb815303e871b6a0e7b7eceae17b883e
grzybek <grzybol.k@gmail.com>
2023-02-20 21:25:41 +0100

    passing backend url in compose

--- frontend/generatorinator/src/routes/generator/[id]/+page.js
+++ frontend/generatorinator/src/routes/generator/[id]/+page.js
@@ -3,6 +3,7 @@ import { env } from '$env/dynamic/public';
 /** @type {import('./$types').PageLoad} */
 export async function load({params}) {
   console.log('load', params.id)
+  console.log('loading from: ' + env.PUBLIC_BACKEND_URL)
   let res = await fetch(env.PUBLIC_BACKEND_URL + '/generator/' + params.id, {
     method: 'GET'
   })
